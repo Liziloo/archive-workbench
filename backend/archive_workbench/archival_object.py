6e2c67e2f924e07c00c6ca5702d6c933040bb52b
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2026-09-06 10:38:54 -0400

    Implement duplicate detection

--- backend/archive_workbench/archival_object.py
+++ backend/archive_workbench/archival_object.py
@@ -2,14 +2,25 @@ from hashlib import sha256
 from pathlib import Path
 
 
+class AddRepresentationResult:
+    def __init__(self, duplicate: bool, added: bool):
+        self.duplicate = duplicate
+        self.added = added
+
+
 class ArchivalObject:
     def __init__(self):
         self.representations = []
 
-    def add_representation(self, path: Path):
-        representation = path
-        self.representations.append(representation)
-        return representation
+    def add_representation(self, path: Path, confirm_duplicate: bool = False):
+        if self.check_for_duplicate(path):
+            if confirm_duplicate:
+                self.representations.append(path)
+                return AddRepresentationResult(duplicate=True, added=True)
+            else:
+                return AddRepresentationResult(duplicate=True, added=False)
+        self.representations.append(path)
+        return path
 
     def check_for_duplicate(self, path: Path) -> bool:
         candidate_hash = sha256(path.read_bytes()).digest()
